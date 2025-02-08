ee936cdebdb1efb55e51b02244e12f8612d6a845
unknown <alannaphung816@gmail.com>
2025-02-07 22:07:18 -0600

    react menu created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import MenuItem from './components/MenuItem';
 
-// import 'bootstrap/dist/css/bootstrap.min.css'; // This imports bootstrap css styles. You can use bootstrap or your own classes by using the className attribute in your elements.
+import 'bootstrap/dist/css/bootstrap.min.css'; // This imports bootstrap css styles. You can use bootstrap or your own classes by using the className attribute in your elements.
 
 // Menu data. An array of objects where each object represents a menu item. Each menu item has an id, title, description, image name, and price.
 // You can use the image name to get the image from the images folder.
@@ -82,13 +82,40 @@ const menuItems = [
 function App() {
   return (
     <div>
-      <h1>Menu</h1>
-      <div className="menu">
-        {/* Display menu items dynamicaly here by iterating over the provided menuItems */}
-        <MenuItem title={menuItems[0].title} /> {/* Example for how to use a component */}
+      <Title title="Japanese Menu" logo="sushiLogo.png"/>
+      <Tagline motto="Sushi that serves" subtitle="we cooked"/>
+      <div>
+        {menuItems.map(({title, description, price, imageName}) =>
+        (
+          <MenuItem title={title} desc={description} price={price} imgname={imageName} />
+        ))}
       </div>
     </div>
   );
+}
+
+function Title({title, logo}) {
+  return (
+  <div class="container-fluid">
+    <div class = "bg-white text-black p-5 text-center">
+      <img src={logo} alt="Logo" class="logo" style={{margin: 5}}></img>
+      <span class="title">{title}</span>
+    </div>
+  </div>);
+}
+
+function Tagline({motto, subtitle}) {
+  return (
+    <div>
+      <div class = "bg-white text-black text-center">
+      <span class="fancytext">{motto}</span>
+    </div>
+
+    <div class = "bg-white text-black mb-5 text-center">
+      <span class="bigtext">{subtitle}</span>
+    </div>
+    </div>
+  )
 }
 
 export default App;
--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 // This is a functional component that represents a single menu item. It currently takes in the title and displays it in an h2 element.
 // Modify the component to take in all the other properties of a menu item you need and display them in the component.
 // Use bootstrap to style the elements so that it looks like the mockup in the assignment.
 // Hint: You can use the image name to get the image from the images folder.
-const MenuItem = ({ title }) => {
+const MenuItem = ({ title, desc, price, imgname }) => {
     return (
-        <div>
-            <h2>{title}</h2>
+        <div class="row p-3">
+            <div class="col-4"><img src={'images/' + imgname} alt={imgname} class="img-fluid"></img></div>
+            <div class="col-8">
+                <div style={{marginRight: '15px', display: 'flex', flexDirection: 'column'}}>
+                    <span class="itemtitle">{title}</span>
+                    <span class="itemdesc">{desc}</span>
+                    <div class="row">
+                        <div class="col"><span class="itemprice">{'$' + price}</span></div>
+                        <div class="col justify-content-end" style={{ display: 'grid', justifyItems: 'end' }}>
+                            <button type="button" class="button" style={{alignSelf: 'flex-end'}}>Add</button>
+                            </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
